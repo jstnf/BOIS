c82e0d61f026436c96a6184ec117cc195b4f47fc
Justin <justinbfigueroa@gmail.com>
2022-08-20 00:16:49 -0700

    Update spacing

--- bois.py
+++ bois.py
@@ -12,17 +12,19 @@ PATH = os.getenv('FILE_PATH')
 
 bot = discord.Bot()
 
+
 @bot.event
 async def on_voice_state_update(member, before, after):
     vc_before = before.channel
     vc_after = after.channel
-    
+
     if not vc_before and int(member.id) == int(MEMBER):
         vc = await vc_after.connect()
         vc.play(discord.FFmpegPCMAudio(PATH))
         with audioread.audio_open(PATH) as f:
-            #Start Playing
-            await sleep(f.duration + 5) # Allow a bit of time for song to load :D
+            # Start Playing
+            await sleep(f.duration + 5)  # Allow a bit of time for song to load :D
         await vc.disconnect()
+
 
 bot.run(TOKEN)
