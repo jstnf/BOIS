ce3c63cc7a5f54c4fe5f2ba393599876a5bbae4d
jstnf <justin@justinf.dev>
2023-05-17 00:33:04 +0000

    Update to use discord.Client, clean requirements

--- bois.py
+++ bois.py
@@ -10,21 +10,21 @@ TOKEN = os.getenv('BOT_TOKEN')
 MEMBER = os.getenv('MEMBER_ID')
 PATH = os.getenv('FILE_PATH')
 
-bot = discord.Bot()
+class MyClient(discord.Client):
+    async def on_voice_state_update(self, member, before, after):
+        vc_before = before.channel
+        vc_after = after.channel
 
+        if not vc_before and int(member.id) == int(MEMBER):
+            vc = await vc_after.connect()
+            vc.play(discord.FFmpegPCMAudio(PATH))
+            with audioread.audio_open(PATH) as f:
+                # Start Playing
+                await sleep(f.duration + 5)  # Allow a bit of time for song to load :D
+            await vc.disconnect()
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    vc_before = before.channel
-    vc_after = after.channel
+intents = discord.Intents.default()
+intents.voice_states = True
 
-    if not vc_before and int(member.id) == int(MEMBER):
-        vc = await vc_after.connect()
-        vc.play(discord.FFmpegPCMAudio(PATH))
-        with audioread.audio_open(PATH) as f:
-            # Start Playing
-            await sleep(f.duration + 5)  # Allow a bit of time for song to load :D
-        await vc.disconnect()
-
-
-bot.run(TOKEN)
+client = MyClient(intents = intents)
+client.run(TOKEN)
