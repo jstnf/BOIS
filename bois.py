e29cc8b90ef31cb4bdd489f35fe799546da3be24
Justin <justin@justinf.dev>
2024-12-30 15:20:02 -0800

    Rename FILE_PATH to SOUND_FILE_PATH

--- bois.py
+++ bois.py
@@ -8,17 +8,17 @@ from asyncio import sleep
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 MEMBER = os.getenv('MEMBER_ID')
-PATH = os.getenv('FILE_PATH')
+SOUND_FILE_PATH = os.getenv('SOUND_FILE_PATH')
 
-class MyClient(discord.Client):
+class BoisBot(discord.Client):
     async def on_voice_state_update(self, member, before, after):
         vc_before = before.channel
         vc_after = after.channel
 
         if not vc_before and int(member.id) == int(MEMBER):
             vc = await vc_after.connect()
-            vc.play(discord.FFmpegPCMAudio(PATH))
-            with audioread.audio_open(PATH) as f:
+            vc.play(discord.FFmpegPCMAudio(SOUND_FILE_PATH))
+            with audioread.audio_open(SOUND_FILE_PATH) as f:
                 # Start Playing
                 await sleep(f.duration + 5)  # Allow a bit of time for song to load :D
             await vc.disconnect()
@@ -26,5 +26,5 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.voice_states = True
 
-client = MyClient(intents = intents)
+client = BoisBot(intents = intents)
 client.run(TOKEN)
